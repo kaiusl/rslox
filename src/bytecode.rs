8a0adbd86851883d79bdd0086503b75ae2c0088a
Kaius Loos <37976028+kaiusl@users.noreply.github.com>
2024-09-02 18:26:18 +0300

    support constants

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,19 +1,39 @@
 use std::borrow::Cow;
-use std::fmt;
+use std::collections::HashMap;
+use std::{fmt, mem};
 
+use crate::common::Span;
 use crate::disassembler::{Disassembler, DisassemblerError};
+use crate::value::Value;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ByteCode {
     pub code: Vec<u8>,
+    pub constants: Vec<Value>,
+    pub spans: HashMap<usize, Span>,
 }
 
 impl ByteCode {
-    pub fn disassemble(&self) -> Disassembler<'_> {
-        Disassembler {
-            bytecode: &self.code,
-            offset: 0,
+    pub fn new() -> Self {
+        Self {
+            code: Vec::new(),
+            constants: Vec::new(),
+            spans: HashMap::new(),
         }
+    }
+
+    pub fn push(&mut self, instruction: Instruction, span: Span) {
+        self.spans.insert(self.code.len(), span);
+        instruction.write_bytes(&mut self.code);
+    }
+
+    pub fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.push(value);
+        (self.constants.len() - 1)
+    }
+
+    pub fn disassemble(&self) -> Disassembler<'_> {
+        Disassembler::new(self)
     }
 }
 
@@ -21,47 +41,64 @@ impl ByteCode {
 #[repr(u8)]
 pub enum OpCode {
     Return,
+    Constant,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Instruction {
     Return,
+    Constant(u8),
 }
 
 impl Instruction {
     pub fn op_code(&self) -> OpCode {
         match self {
             Instruction::Return => OpCode::Return,
+            Instruction::Constant(_) => OpCode::Constant,
         }
     }
 
     pub fn write_bytes(&self, dst: &mut Vec<u8>) {
         dst.push(self.op_code() as u8);
+        // push operands
         match self {
             Instruction::Return => {}
+            Instruction::Constant(idx) => {
+                dst.push(*idx);
+            }
         }
     }
 
     pub fn from_bytes(bytes: &[u8]) -> OpFromBytesResult<'_> {
-        match bytes.first() {
-            Some(&b) if b == OpCode::Return as u8 => OpFromBytesResult {
-                op: Ok(Instruction::Return),
-                offset: 1,
-                remainder: bytes.get(1..).unwrap_or_default(),
-            },
-            _ => OpFromBytesResult {
-                op: Err(DisassemblerError {
-                    message: Cow::Borrowed("Unknown opcode"),
-                }),
-                offset: 0,
-                remainder: bytes,
-            },
+        let instr = match bytes.first() {
+            Some(&b) if b == OpCode::Return as u8 => Instruction::Return,
+            Some(&b) if b == OpCode::Constant as u8 => {
+                let idx = bytes[1];
+                Instruction::Constant(idx)
+            }
+            _ => {
+                return OpFromBytesResult {
+                    op: Err(DisassemblerError {
+                        message: Cow::Borrowed("Unknown opcode"),
+                    }),
+                    offset: 0,
+                    remainder: bytes,
+                }
+            }
+        };
+
+        let offset = instr.byte_len();
+        OpFromBytesResult {
+            op: Ok(instr),
+            offset,
+            remainder: &bytes[offset..],
         }
     }
 
     pub fn byte_len(&self) -> usize {
         1 + match self {
             Instruction::Return => 0,
+            Instruction::Constant(idx) => mem::size_of_val(idx),
         }
     }
 }
@@ -70,6 +107,7 @@ impl fmt::Display for Instruction {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Instruction::Return => write!(f, "RETURN"),
+            Instruction::Constant(idx) => write!(f, "CONSTANT {}", idx),
         }
     }
 }
--- src/disassembler.rs
+++ src/disassembler.rs
@@ -1,25 +1,52 @@
 use std::borrow::Cow;
 
-use crate::bytecode::Instruction;
+use crate::bytecode::{ByteCode, Instruction};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Disassembler<'a> {
-    pub bytecode: &'a [u8],
+    pub bytecode: &'a ByteCode,
+    pub bytes: &'a [u8],
     pub offset: usize,
 }
 
 impl Disassembler<'_> {
-    pub fn new(bytecode: &[u8]) -> Disassembler<'_> {
+    pub fn new(bytecode: &ByteCode) -> Disassembler<'_> {
         Disassembler {
             bytecode,
+            bytes: &bytecode.code,
             offset: 0,
         }
     }
 
-    pub fn print(mut self) {
+    pub fn print(self) {
+        let constants = &self.bytecode.constants;
+        let spans = &self.bytecode.spans;
+        let mut prev_line = None;
         for result in self {
             match result {
-                Ok((offset, op)) => println!("{:04} {:?}", offset, op),
+                Ok((offset, op)) => {
+                    let line = &spans[&offset].line;
+                    'print_line: {
+                        if let Some(prev_line) = prev_line {
+                            if prev_line == line {
+                                print!("   |");
+                                break 'print_line;
+                            }
+                        }
+
+                        print!("{:04}", line);
+                    }
+                    prev_line = Some(line);
+                    print!(" {:04} {}", offset, op);
+                    match op {
+                        Instruction::Constant(idx) => {
+                            print!(" ({})", &constants[idx as usize]);
+                        }
+                        _ => {}
+                    }
+
+                    println!();
+                }
                 Err(err) => println!("Error: '{}'", err.message),
             }
         }
@@ -30,11 +57,11 @@ impl<'a> Iterator for Disassembler<'a> {
     type Item = Result<(usize, Instruction), DisassemblerError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.bytecode.is_empty() {
+        if self.bytes.is_empty() {
             None
         } else {
-            let result = Instruction::from_bytes(self.bytecode);
-            self.bytecode = result.remainder;
+            let result = Instruction::from_bytes(self.bytes);
+            self.bytes = result.remainder;
             match result.op {
                 Ok(op) => {
                     let ret = Some(Ok((self.offset, op)));
@@ -54,21 +81,27 @@ pub struct DisassemblerError {
 
 #[cfg(test)]
 mod tests {
-    use crate::bytecode::OpCode;
+    use crate::bytecode::ByteCode;
+    use crate::common::Span;
+    use crate::value::Value;
 
     use super::*;
 
     #[test]
     fn it_disassembles() {
-        let mut disassembler = Disassembler {
-            bytecode: &[OpCode::Return as u8, OpCode::Return as u8],
-            offset: 0,
-        };
+        let mut code = ByteCode::new();
+        code.push(Instruction::Return, Span::new(0, 0, 5));
+        let idx = code.add_constant(Value::Number(1.254));
+        code.push(Instruction::Constant(idx as u8), Span::new(1, 6, 8));
+        code.push(Instruction::Return, Span::new(1, 9, 12));
+
+        let mut disassembler = code.disassemble();
 
         disassembler.clone().print();
 
         assert_eq!(disassembler.next(), Some(Ok((0, Instruction::Return))));
-        assert_eq!(disassembler.next(), Some(Ok((1, Instruction::Return))));
+        assert_eq!(disassembler.next(), Some(Ok((1, Instruction::Constant(0)))));
+        assert_eq!(disassembler.next(), Some(Ok((3, Instruction::Return))));
         assert_eq!(disassembler.next(), None);
     }
 }
