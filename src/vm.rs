f204d77e9ffc2c1eb53c638e614b4aaabbcfae7d
Kaius Loos <37976028+kaiusl@users.noreply.github.com>
2024-09-02 21:08:08 +0300

    setup stack

--- src/vm.rs
+++ src/vm.rs
@@ -9,10 +9,13 @@ use self::error::InterpretError;
 
 pub mod error;
 
+type Stack<T> = Vec<T>;
+
 pub struct Vm {
     pub constants: Vec<Value>,
     pub spans: HashMap<usize, Span>,
     pub instructions: ByteCursor,
+    pub stack: Stack<Value>,
 
     #[cfg(feature = "debug_trace")]
     pub disassembler: Option<Disassembler>,
@@ -24,6 +27,7 @@ impl Vm {
             constants: Vec::new(),
             spans: HashMap::new(),
             instructions: ByteCursor::new(Vec::new()),
+            stack: Stack::new(),
 
             #[cfg(feature = "debug_trace")]
             disassembler: None,
@@ -46,16 +50,26 @@ impl Vm {
         while let Some(op) = self.instructions.u8().map(OpCode::from_u8) {
             #[cfg(feature = "debug_trace")]
             {
+                println!("\n/ [");
+                for slot in self.stack.iter().rev() {
+                    println!("/   {slot}");
+                }
+                println!("/ ]");
                 if let Some(disassembler) = &mut self.disassembler {
+                    print!("/ ");
                     disassembler.print_next();
                 }
             }
 
             match op {
-                OpCode::Return => return Ok(()),
+                OpCode::Return => {
+                    let value = self.stack.pop().unwrap();
+                    println!("{}", value);
+                }
                 OpCode::Constant => {
                     let index = self.instructions.u8().unwrap();
                     let value = &self.constants[index as usize];
+                    self.stack.push(value.clone());
 
                     println!("{}", value);
                 }
