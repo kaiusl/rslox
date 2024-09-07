69cd22e4fb94654181ee0da14c29ba34170347e9
Kaius Loos <37976028+kaiusl@users.noreply.github.com>
2024-09-07 16:31:57 +0300

    update benches to run each test 3 times

--- bench.sh
+++ bench.sh
@@ -1,13 +1,21 @@
 cargo build --release
 
 echo "equality"
+for v in {1..3}
+do
 ./target/release/rslox -f ./crafting_interpreters_test_files/benchmark.ignore/equality.lox
+done
 
 echo ""
 echo "fib"
+for v in {1..3}
+do
 ./target/release/rslox -f ./crafting_interpreters_test_files/benchmark.ignore/fib.lox
-
+done
 
 echo ""
 echo "string_equality"
+for v in {1..3}
+do
 ./target/release/rslox -f ./crafting_interpreters_test_files/benchmark.ignore/string_equality.lox
+done
