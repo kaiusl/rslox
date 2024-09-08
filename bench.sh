81d94e88a6b032cfe663213a28403aaa23460b58
Kaius Loos <37976028+kaiusl@users.noreply.github.com>
2024-09-08 10:34:40 +0300

    add new benchmarks

--- bench.sh
+++ bench.sh
@@ -1,9 +1,10 @@
 cargo build --release
 
-for path in "equality" "fib" "string_equality"
+for path in "binary_trees" "equality" "fib" "instantiation" "invocation" "properties" "string_equality" "trees" "zoo_batch" "zoo"
 do 
     echo ""
     echo $path
+    echo ""
     for ((i=0; i<${1:-1}; i++))
     do
         ./target/release/rslox -f ./crafting_interpreters_test_files/benchmark.ignore/$path.lox
